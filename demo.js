c288ce408c54efbc8d08e03dee19231a77ace5e9
W-Dragoner <202091630237@lyu.edu.cn>
2024-01-23 10:49:15 +0800

    one

---
+++ demo.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2024-01-23
+// @description  try to take over the world!
+// @author       You
+// @match        https://blog.csdn.net/m0_72594605/article/details/132559545
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=csdn.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
